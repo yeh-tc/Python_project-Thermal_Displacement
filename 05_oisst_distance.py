d6644d78e384b9daa9b60f201b25aa05aa616104
Ting-Chen Yeh <98454075+yeh-tc@users.noreply.github.com>
2023-10-03 09:54:38 +0800

    Update 05_oisst_distance.py

--- 05_oisst_distance.py
+++ 05_oisst_distance.py
@@ -9,20 +9,30 @@ lat = f_grid['lat'].values
 Re = 6371  # Earth's radius in km
 res = lon[1] - lon[0]  # Get grid parameters for calculation
 
-lats = np.arange(np.deg2rad(min(lat)), np.deg2rad(max(lat) + res), np.deg2rad(res))
-dlon = np.arange(np.deg2rad(min(lon) - max(lon)), np.deg2rad(max(lon) - min(lon) + res), np.deg2rad(res))
+lats = np.arange(lat.min(), lat.max() + res, res)
+dlon = np.concatenate([lon[-1] - lon[:-1] + res, lon])
+lat_mat, dlon_mat = np.meshgrid(lats, dlon)
+d = np.empty((len(lats), len(dlon), len(lats)))
+start_time = time.time()
+for ilat, current_lat in enumerate(lats):
+        # Calculate distance to all other points
+    d[ilat, :, :] = Re * np.arccos(
+        np.sin(np.deg2rad(current_lat)) * np.sin(np.deg2rad(lat_mat)) +
+        np.cos(np.deg2rad(current_lat)) * np.cos(np.deg2rad(lat_mat)) * np.cos(np.deg2rad(dlon_mat))
+    )
 
-lat_mat = np.tile(lats, (len(dlon), 1))
-dlon_mat = np.tile(dlon, (len(lats), 1)).T
+    # Periodically report status
+    if ilat % (len(lats) // 20) == 0:
+        elapsed_time = (time.time() - start_time) / 60
+        print(f"{100 * ilat / len(lats):.0f}% done, {elapsed_time:.0f} min elapsed")
+
+    # Convert the array to xarray DataArray for saving
+d_da = xr.DataArray(
+    d.astype(np.float32),
+    coords={"lats": lats, "dlon": dlon, "lat": lats},
+    dims=["lats", "dlon", "lat"]
+)
+d_da.to_netcdf(fout)
 
 d = np.zeros((len(lats), len(dlon), len(lats)))
-
-t1 = time.time()
-
-for ilat, lat_val in enumerate(lats):
-    d[ilat, :, :] = Re * np.arccos(np.sin(lat_val) * np.sin(lat_mat) + np.cos(lat_val) * np.cos(lat_mat) * np.cos(dlon_mat))
-
-df = xr.DataArray(d, coords=[('lats', lats), ('dlon', dlon), ('lat', lats)])
-df.to_netcdf('oisst_distance2.nc')
-
-print("Time elapsed: ", time.time() - t1)
+d_da.to_netcdf('oisst_distance.nc')
